93533bb9fcb3b319402d104616fe7541eac3e0cf
Daniel Jones <messyjones@gmail.com>
2016-10-09 19:45:12 -0400

    made the tag function to pass users

--- cloud/main.js
+++ cloud/main.js
@@ -1,8 +1,30 @@
 
-Parse.Cloud.define('hello', function(req, res) {
-    res.success('Holo');
-});
-
-Parse.Cloud.define('pizza', function(req, res) {
-    res.success('Buddha');
+Parse.Cloud.define("getUsersAndTheirTags", function(request, response) {
+    var query = new Parse.Query("ParseTag");
+    var tagTitles = request.params.tagTitles;
+    query.containedIn("title", tagTitles);
+    query.inlude("createdBy");
+  query.find({
+    success: function(results) {
+    //the necessaryTagCount means that a user must have this number of tags to be a chosen user, because a chosen user needs to have all searched tags.
+    var necessaryTagCount = tagTitles.length;
+    var userTagRatioDictionary = {};
+    var usersToPass = [];
+      for (var i = 0; i < results.length; ++i) {
+          var user = results[i].get("createdBy");
+          if user in userTagRatioDictionary {
+              userTagRatioDictionary[user] = userTagRatioDictionary[user] + 1;
+              if userTagRatioDictionary[user] == necessaryTagCount {
+                  usersToPass.push(user);
+              }
+          } else {
+              userTagRatioDictionary[user] = 1;
+          }
+      }
+      response.success(usersToPass);
+    },
+    error: function() {
+      response.error("movie lookup failed");
+    }
+  });
 });
