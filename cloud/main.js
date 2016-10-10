6e9f0741a8c409526017f8427c50ae20c5ec790a
Daniel Jones <messyjones@gmail.com>
2016-10-10 09:42:56 -0400

    changed the masterKey string

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,8 @@
 Parse.Cloud.define("hobo", function(request, response) {
   var query = new Parse.Query("ParseSwipe");
 //  query.equalTo("userOneApproval", request.params.bool);
-  query.find({ useMasterKey: true }).then(function(results) {
+  query.find({ useMasterKey : true }).then(
+      function(results) {
+      response.success("helllo");
   });
 });
