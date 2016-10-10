eac44e78dd60ac5abfea53fd27c9f9cdf78d205a
Daniel Jones <messyjones@gmail.com>
2016-10-10 08:47:12 -0400

    made a hobo function

--- cloud/main.js
+++ cloud/main.js
@@ -1,1 +1,12 @@
-
+Parse.Cloud.define("hobo", function(request, response) {
+  var query = new Parse.Query("User");
+  query.equalTo("fullName", request.params.fullName);
+  query.find({
+    success: function(results) {
+      response.success(results.length);
+    },
+    error: function() {
+      response.error("movie lookup failed");
+    }
+  });
+});
