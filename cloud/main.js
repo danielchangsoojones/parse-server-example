0208c27a52d17528dfa93870b97ebd6d37a03698
Daniel Jones <messyjones@gmail.com>
2016-10-10 08:41:02 -0400

    deleted all cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -1,14 +1,1 @@
 
-Parse.Cloud.define("averageStars", function(request, response) {
-  var query = new Parse.Query("User");
-    let userId = request.params.userId;
-  query.equalTo("objectId", userId);
-  query.find({
-    success: function(results) {
-      response.success(results.item[0]);
-    },
-    error: function() {
-      response.error("movie lookup failed");
-    }
-  });
-});
