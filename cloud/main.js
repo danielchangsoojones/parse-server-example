c472bf655b293284feb54d701f17644f8400882e
Daniel Jones <messyjones@gmail.com>
2016-10-10 10:42:19 -0400

    put in get UsersAndTheirTags function

--- cloud/main.js
+++ cloud/main.js
@@ -1,12 +1,29 @@
-Parse.Cloud.define("hobo", function(request, response) {
-  var query = new Parse.Query("ParseSwipe");
-//  query.equalTo("userOneApproval", request.params.bool);
-  query.find({ useMasterKey : true }).then(
-      function(results) {
-      response.success(results);
-  },
-    function(error) {
-        response.error(error);
-        console.log(error);
-});
+Parse.Cloud.define("getUsersAndTheirTags", function(request, response) {
+    var query = new Parse.Query("ParseTag");
+    var tagTitles = request.params.tagTitles;
+    query.containedIn("title", tagTitles);
+    query.include("createdBy");
+    query.find({ useMasterKey : true }).then(
+        function(results) {
+            var necessaryTagCount = tagTitles.length;
+            var userTagRatioDictionary = {};
+            var usersToPass = [];
+            for (var i = 0; i < results.length; ++i) {
+                var user = results[i].get("createdBy");
+                if user in userTagRatioDictionary {
+                    userTagRatioDictionary[user] = userTagRatioDictionary[user] + 1;
+                    if userTagRatioDictionary[user] == necessaryTagCount {
+                        usersToPass.push(user);
+                    }
+                } else {
+                    userTagRatioDictionary[user] = 1;
+                }
+            }
+            response.success(usersToPass);
+        },
+        function(error) {
+            response.error(error);
+            console.log(error);
+        }
+  );
 });
