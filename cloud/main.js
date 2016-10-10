4a960d83223e7c69aca9652fbf5d9a150017b799
Daniel Jones <messyjones@gmail.com>
2016-10-10 08:57:26 -0400

    got rid of equal to

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,6 @@
 Parse.Cloud.define("hobo", function(request, response) {
   var query = new Parse.Query("ParseSwipe");
-  query.equalTo("userOneApproval", request.params.bool);
+//  query.equalTo("userOneApproval", request.params.bool);
   query.find({
     success: function(results) {
       response.success(results.length);
