fa03a6883fb16f8c6d2d8911d558b5edb395125c
Daniel Jones <messyjones@gmail.com>
2016-10-10 08:17:45 -0400

    made average star function

--- cloud/main.js
+++ cloud/main.js
@@ -28,3 +28,17 @@ Parse.Cloud.define("getUsersAndTheirTags", function(request, response) {
     }
   });
 });
+
+Parse.Cloud.define("averageStars", function(request, response) {
+  var query = new Parse.Query("User");
+    let userId = request.params.userIds[0];
+  query.equalTo("objectId", userId);
+  query.find({
+    success: function(results) {
+      response.success(results.item[0]);
+    },
+    error: function() {
+      response.error("movie lookup failed");
+    }
+  });
+});
