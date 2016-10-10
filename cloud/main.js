c31a7990f68222ac78b1971d73f7c7196481e975
Daniel Jones <messyjones@gmail.com>
2016-10-10 09:21:15 -0400

    added in just success

--- cloud/main.js
+++ cloud/main.js
@@ -1,12 +1,7 @@
 Parse.Cloud.define("hobo", function(request, response) {
   var query = new Parse.Query("ParseSwipe");
 //  query.equalTo("userOneApproval", request.params.bool);
-  query.find({ useMasterKey: true }).then({
-    success: function(results) {
+  query.find({ useMasterKey: true }).then(function(results) {
       response.success(results.length);
-    },
-    error: function() {
-      response.error("movie lookup failed");
-    }
   });
 });
