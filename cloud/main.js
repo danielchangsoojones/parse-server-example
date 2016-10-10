a3603d5ad7b885704e4ed7bcc6353639a1f3b00e
Daniel Jones <messyjones@gmail.com>
2016-10-10 09:25:26 -0400

    changed results.length

--- cloud/main.js
+++ cloud/main.js
@@ -2,6 +2,5 @@ Parse.Cloud.define("hobo", function(request, response) {
   var query = new Parse.Query("ParseSwipe");
 //  query.equalTo("userOneApproval", request.params.bool);
   query.find({ useMasterKey: true }).then(function(results) {
-      response.success(results.length);
   });
 });
