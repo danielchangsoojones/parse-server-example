6e911f44db934aeaaacb847ca65976defcb94f5d
Daniel Jones <messyjones@gmail.com>
2016-10-28 11:13:05 -0400

    tried to do some logging

--- cloud/main.js
+++ cloud/main.js
@@ -2,13 +2,16 @@
 Parse.Cloud.define("saveOtherUser", function(request, response) {
     var query = new Parse.Query(Parse.User);
     query.equalTo("objectId", request.params.targetObjectId);
+    console.log("hello");
     query.find({useMasterKey : true}).then(function(results) {
     // process the result of the query here
     // Save the user object
-        var user = results[0];
-        console.log("hi");
-        console.log(user);
-        
+        console.log("echoooo");
+        return "hi";
+//        var user = results[0];
+//        
+//        console.log(user);
+//        
 //        success: function(results) {
 //            var obj = results[0];
 //                obj.save(null,{
