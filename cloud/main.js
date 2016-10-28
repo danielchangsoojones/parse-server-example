dec136f6870948fba3594d6f26a23692b0f7a9a8
Daniel Jones <messyjones@gmail.com>
2016-10-28 10:42:57 -0400

    commented out the user function...

--- cloud/main.js
+++ cloud/main.js
@@ -1,24 +1,24 @@
 
-Parse.Cloud.define("saveOtherUser", function(request, response) {
-    var query = new Parse.Query(Parse.User);
-    query.equalTo("objectId", request.params.targetObjectId);
-    query.find({useMasterKey : true}).then(function(results) {
-    // process the result of the query here
-    // Save the user object
-        success: function(results) {
-            var obj = results[0];
-                obj.save(null,{
-                  success: function (object) { 
-                    response.success(object);
-                  }, 
-                error: function (object, error) { 
-                  response.error(error);
-                }
-              });
-        },
-        error: function(error) {
-            console.log("failed");
-        }
-
-    });
-  });
+//Parse.Cloud.define("saveOtherUser", function(request, response) {
+//    var query = new Parse.Query(Parse.User);
+//    query.equalTo("objectId", request.params.targetObjectId);
+//    query.find({useMasterKey : true}).then(function(results) {
+//    // process the result of the query here
+//    // Save the user object
+//        success: function(results) {
+//            var obj = results[0];
+//                obj.save(null,{
+//                  success: function (object) { 
+//                    response.success(object);
+//                  }, 
+//                error: function (object, error) { 
+//                  response.error(error);
+//                }
+//              });
+//        },
+//        error: function(error) {
+//            console.log("failed");
+//        }
+//
+//    });
+//  });
